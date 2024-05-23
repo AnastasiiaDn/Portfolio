9e0fcced0aaac6e7ea4cb4d76f5c83aa08057e4c
AnastasiiaDn <anastasiia.denesiuk@elev.ga.ntig.se>
2024-05-23 15:56:50 +0200

    inirtusl

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello World!")
+function myFunction() 
+    {document.getElementById("control").style.fontSize = "30px";
+    document.getElementById("control").style.color = "white";
+    document.getElementById("control").style.backgroundColor = "green";
+    document.getElementById("bite").style.color = "red"
+    document.getElementById("bite").style.backgroundColor = "black"
+    document.getElementById("bomsh").style.color = "blue"
+    document.getElementById("bomsh").style.backgroundColor = "purple"
+   
+    }
+
+
+function color()
+{
+    document.getElementById("bi").style.backgroundColor = "red"
+    document.getElementById("bi").style.color = "black"
+
+}
+
+
